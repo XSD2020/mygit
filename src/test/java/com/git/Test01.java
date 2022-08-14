78c145c74d900c5a8ee62bb9d304bc898a8e61d4
xieweida <1453150399@qq.com>
2022-08-14 22:31:26 +0800

    修改测试内容

--- src/test/java/com/git/Test01.java
+++ src/test/java/com/git/Test01.java
@@ -3,5 +3,7 @@ package com.git;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("Test01.main");
+        System.out.println("Test02.main");
+        System.out.println("Test03.main");
     }
 }
